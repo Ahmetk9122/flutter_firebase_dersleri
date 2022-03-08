1f850882c55829413a557ff07c4e68d70546251f
Ahmet Karabudak <ahmet.karabudakk.9122@gmail.com>
2022-03-08 13:02:03 +0300

    veriOkuRealTime - streamDurdur - batchKavrami

--- lib/firestore_islemler.dart
+++ lib/firestore_islemler.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class FirestoreIslemleri extends StatelessWidget {
   FirestoreIslemleri({Key? key}) : super(key: key);
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  StreamSubscription? _userSubscribe;
   @override
   Widget build(BuildContext context) {
     //IDler
@@ -36,11 +39,31 @@ class FirestoreIslemleri extends StatelessWidget {
                   veriSil();
                 },
                 child: Text("Veri Sil")),
-             ElevatedButton(
+            ElevatedButton(
                 onPressed: () {
                   verileriOku();
                 },
                 child: Text("Verileri Oku")),
+            ElevatedButton(
+                onPressed: () {
+                  veriOkuRealTime();
+                },
+                child: Text("Verileri Gerçek Zamanlı Oku")),
+            ElevatedButton(
+                onPressed: () {
+                  streamDurdur();
+                },
+                child: Text("Gerçek Zamanlı Okumayı Durdur")),
+            ElevatedButton(
+                onPressed: () {
+                  batchKavrami();
+                },
+                child: Text("Batch Kavrami")),
+            ElevatedButton(
+                onPressed: () {
+                  transactionKavrami();
+                },
+                child: Text("Transaction Kavrami")),
           ],
         ),
       ),
@@ -75,12 +98,10 @@ class FirestoreIslemleri extends StatelessWidget {
   }
 
   Future<void> veriGuncelle() async {
-     await _firestore.doc("users/VRlWFZVsZPJkKlTPRSMA").update(
-       {
-         "adres.ilce":"fatih", 
-         "adres.il":"Kayseri", 
-       }
-     );
+    await _firestore.doc("users/VRlWFZVsZPJkKlTPRSMA").update({
+      "adres.ilce": "fatih",
+      "adres.il": "Kayseri",
+    });
   }
 
   Future<void> veriSil() async {
@@ -88,26 +109,79 @@ class FirestoreIslemleri extends StatelessWidget {
     await _firestore.doc("users/tE7C9AmkRCBrLEgVWKyg").delete();
 
     //koleksiyon içindeki bir sütnun silme
-    await _firestore.doc("users/KxcNVExo8sE8pe7c8jSE").update(
-      {
-        "araba":FieldValue.delete(),
-      }
-    );
+    await _firestore.doc("users/KxcNVExo8sE8pe7c8jSE").update({
+      "araba": FieldValue.delete(),
+    });
   }
 
   Future<void> verileriOku() async {
     var _usersDocument = await _firestore.collection("users").get();
     debugPrint(_usersDocument.size.toString());
     debugPrint(_usersDocument.docs.length.toString());
-    for(var eleman in _usersDocument.docs)
-    {
+    for (var eleman in _usersDocument.docs) {
       print("Döküman id ${eleman.id}");
 
       Map userMap = eleman.data();
       print(userMap["isim"]);
     }
-     var _emreDoc =await _firestore.doc("users/VRlWFZVsZPJkKlTPRSMA").get();
-     print(_emreDoc.data().toString());
-     print(_emreDoc.data()!["adres"]["ilce"]);
+    var _emreDoc = await _firestore.doc("users/VRlWFZVsZPJkKlTPRSMA").get();
+    print(_emreDoc.data().toString());
+    print(_emreDoc.data()!["adres"]["ilce"]);
   }
+
+  Future<void> veriOkuRealTime() async {
+    //user içersiindeki tek bir elemanı dinleme.
+    var _userDocStream =
+        await _firestore.doc("users/VRlWFZVsZPJkKlTPRSMA").snapshots();
+    _userSubscribe = _userDocStream.listen((event) {
+      print(event.data().toString());
+    });
+    //bütün useeri dinleme
+
+    //docChanges methodu ile stream dinleme
+    /* var userStream = await _firestore.collection("users").snapshots();
+    _userSubscribe = userStream.listen((event) {
+    /*  event.docChanges.forEach((element) {
+        print(element.doc.data().toString());
+      });*/*/
+    //doc methoduyla stream okuma
+    /*
+      event.docs.forEach((element) {
+        print(element.data().toString());
+      });
+    });
+    */
+  }
+
+  Future<void> streamDurdur() async {
+    await _userSubscribe?.cancel();
+  }
+
+  Future<void> batchKavrami() async {
+    WriteBatch _batch = _firestore.batch();
+    CollectionReference _counterColRef = _firestore.collection("counter");
+    /*Batch ile eleman ekleme.
+    for(int i = 0 ; i<100;i++)
+    {
+      var yeniDoc = _counterColRef.doc();
+      _batch.set(yeniDoc, {"sayac":++i , "id":yeniDoc.id});
+    }
+    */
+    //Batch ile toplu güncelleme yapma
+    /*
+    var _counterDocs = await _counterColRef.get();
+    _counterDocs.docs.forEach((element) {
+      _batch.update(element.reference,{"createdAt":FieldValue.serverTimestamp()});
+    });
+    */
+    //BATCH VERİ SİLME
+    var _counterDocs = await _counterColRef.get();
+    _counterDocs.docs.forEach((element) {
+      _batch.delete(element.reference);
+    });
+
+    await _batch.commit();
+  }
+
+  void transactionKavrami() { }
 }
